868709713f959529849e70dcf8d73c45c69c38ae
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-05-22 12:51:07 -0400

    Fill out about dialog

--- cube_inverter.js
+++ cube_inverter.js
@@ -1,9 +1,9 @@
 /*
 To do:
 - Fix UVs of inverted cubes
-- Populate about dialog
 - Fix some incomplete plugin meta fields
 - Finalize icon
+- Add keybinding to action
 */
 
 (async function() {
@@ -15,9 +15,9 @@ To do:
 
         // Used for about dialog
         const links = {
-                website: "https://google.com/",
+                website: "https://sirjain0.github.io/",
                 twitter: "https://twitter.com/SirJain2",
-                discord: "https://discord.com/"
+                discord: "https://discord.gg/wM4CKTbFVN"
         }
 
         // Registers plugin data
@@ -115,15 +115,26 @@ To do:
                                 ${banner ? `<div id="banner">This window can be reopened at any time from <strong>Help > About Plugins > ${name}</strong></div>` : ""}
                                 <div id="content">
                                         <h1 style="margin-top:-10px">${name}</h1>
-                                        <p>placeholder</p>
+                                        <p>Adds a button that inverts selected cube sizes.</p>
+                                        <h4>Worth noting:</h4>
+                                        <p>- The plugin inverts each value of all the cubes selected - Positive to Negative or Negative to Positive.</p>
+                                        <p>- This plugin logically works on cubes - no meshes or other outliner elements.</p>
+                                        <p>- Like all other keybindings, the keybind tied to the 'Invert Cube' action can be changed in the settings.</p>
+                                        <h4>How to use:</h4>
+                                        <p>To use this plugin, press the <b>Invert Cubes</b> button which is located next to the Inflation number in the edit tab.</p>
+                                        <br>
                                         <div class="socials">
                                                 <a href="${links["website"]}" class="open-in-browser">
                                                         <i class="icon material-icons" style="color:#33E38E">language</i>
-                                                        <label>By ${author}</label>
+                                                        <label>Website</label>
                                                 </a>
                                                 <a href="${links["discord"]}" class="open-in-browser">
                                                         <i class="icon fab fa-discord" style="color:#727FFF"></i>
                                                         <label>Discord Server</label>
+                                                </a>
+                                                <a href="${links["twitter"]}" class="open-in-browser">
+                                                        <i class="fa-brands fa-twitter" style="color:#1DA1F2"></i>
+                                                        <label>Twitter</label>
                                                 </a>
                                         </div>
                                 </div>
